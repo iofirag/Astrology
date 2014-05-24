29ce4f230f2acfaaeedd8e06346c873fddad660b
avishayhajbi <avishayhajbi@gmail.com>
2014-05-24 21:28:43 +0300

    finish location function

--- includes/js/location.js
+++ includes/js/location.js
@@ -1,3 +1,7 @@
+$(document).ready(function() {
+	getCountryName(); //ip location
+	getLocation(); //html5 location
+});
 function getCountryName() {
 	$.get("http://ipinfo.io/" + myip, function(response) {
 		//console.log(response);
@@ -6,33 +10,42 @@ function getCountryName() {
 		console.log("IP position " + myip + "- " + response.city);
 	}, "jsonp");
 }
-$(document).ready(function() {
-	//ip location
-	getCountryName();
-	//html5 location
-	getLocation();
-});
 function getLocation() {
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(showPosition);
+		navigator.geolocation.watchPosition(showPosition, errorHandler);
 	} else {
 		alert("Geolocation is not supported by this browser.");
 	}
 }
-
 function showPosition(position) {
 	console.log("HTML5 position " + position.coords.latitude + "    " + position.coords.longitude);
 	var latitude = position.coords.latitude;
 	var longitude = position.coords.longitude;
-	apiKEY = "AIzaSyDX_ntczz6MggSUcA8d0q7yMrI5Y2TB07g";
-	locAPI = "https://maps.googleapis.com/maps/api/geocode/json?address=Israel&bounds="+latitude+","+longitude+"|"+ latitude+","+longitude+"&sensor=false&key="+apiKEY;
 	$.ajax({
-		type : 'GET',
-		url : locAPI,
-		dataType : 'json',
-		async : false,
+		url : 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true',
 		success : function(data) {
-			console.log(data);
-		}
-	});
+			console.log(data.results[0].formatted_address);
+			/*or you could iterate the components for only the city and state*/
+			for ( i = 0; i < data.results[4].address_components.length; i++) {
+				for ( j = 0; j < data.results[4].address_components[i].types.length; j++) {
+					if (data.results[4].address_components[i].types[j] == 'country') {
+						var country_code = data.results[4].address_components[i].long_name;
+						console.log(country_code);
+	}}}}});
+}
+function errorHandler(error) {
+	switch(error.code) {
+		case error.PERMISSION_DENIED:
+			alert("User denied the request for Geolocation.");
+			break;
+		case error.POSITION_UNAVAILABLE:
+			alert("Location information is unavailable.");
+			break;
+		case error.TIMEOUT:
+			alert("The request to get user location timed out.");
+			break;
+		case error.UNKNOWN_ERROR:
+			alert("An unknown error occurred.");
+			break;
+	}
 }
