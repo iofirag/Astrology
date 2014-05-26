ae705928bf85f61562c10538a35958190202c97a
avishayhajbi <avishayhajbi@gmail.com>
2014-05-26 09:55:22 +0300

    astrology translate update -- not finished

--- includes/js/astrology.js
+++ includes/js/astrology.js
@@ -29,26 +29,28 @@ function initDatePicker() {
 	currYear = now.getFullYear();
 	today = (currYear) + "-" + (currMonth) + "-" + (currDay);
 	$('#date').val(today);
-	if (currDay < 10) // day
+	if (currDay < 10)// day
 		currDay = parseInt("10", currDay);
-	if (currMonth < 10) // month
+	if (currMonth < 10)// month
 		currMonth = parseInt("10", currMonth);
 	//alert(currDay);alert(currMonth);alert(currYear);
 }
 
 function separateDatePickerValues() {
 	date = $('#date').val().split("-");
-	date[0] = new Date().getFullYear(); // year
-	if (date[1] < 10) // day
+	date[0] = new Date().getFullYear();
+	// year
+	if (date[1] < 10)// day
 		date[1] = parseInt("10", date[1]);
-	if (date[2] < 10) // month
+	if (date[2] < 10)// month
 		date[2] = parseInt("10", date[2]);
 	xmlLoader();
-	return false; // should be TRUE to procceed
+	return false;
+	// should be TRUE to procceed
 }
 
 function xmlLoader() {
-	astrologyUrl = "http://www.findyourfate.com/rss/horoscope-astrology-feed.asp?mode=view&todate="+currMonth+"/"+currDay+"/"+currYear;
+	astrologyUrl = "http://www.findyourfate.com/rss/horoscope-astrology-feed.asp?mode=view&todate=" + currMonth + "/" + currDay + "/" + currYear;
 	$.ajax({
 		url : astrologyUrl,
 		dataType : "xml",
@@ -58,20 +60,25 @@ function xmlLoader() {
 			// This is the part xml2Json comes in.
 			var JSONConvertedXML = $.xml2json(myXML);
 			//console.log(JSONConvertedXML);
-			var zodiac = getZodiac(date[2],date[1]); // return the zodiac sign
+			var zodiac = getZodiac(date[2], date[1]);
+			// return the zodiac sign
 			for (var i = 0; i < JSONConvertedXML.channel.item.length; i++) {
-				if (JSONConvertedXML.channel.item[i].title.indexOf(zodiac) != -1){
+				if (JSONConvertedXML.channel.item[i].title.indexOf(zodiac) != -1) {
 					console.log(JSONConvertedXML.channel.item[i].title);
 					console.log(JSONConvertedXML.channel.item[i].description);
+					translateText(JSONConvertedXML.channel.item[i].description);
+					// didn't work correctly yet
 					break;
 				}
-				//translateText(JSONConvertedXML.channel.item[i].description); // didn't work correctly yet
 			}
 		}
 	});
 }
 
+var translateKey = "AIzaSyCClt5fo3FDMAym59FQw-R5D39eaTNfWIg";
+//AIzaSyAbgbFfwk-N0xLieZryYOknsJn0jWGS0qA
 function translateText(text) {
+
 	$.ajax({
 		url : "http://ajaxhttpheaders.appspot.com",
 		dataType : 'jsonp',
@@ -80,33 +87,119 @@ function translateText(text) {
 			//console.log("browser language"+language);
 		}
 	});
-	//Call the Google API
-	$.ajax({
-		type : "GET",
-		url : "https://ajax.googleapis.com/ajax/services/language/translate",
-		dataType : 'jsonp',
-		cache : false,
-		contentType : "application/x-www-form-urlencoded; charset=UTF-8",
-		data : "v=1.0&q=" + text + "&langpair=en|iw",
-		success : function(iData) {
-			console.log("****translated**** "+text + " *************");
-		},
-		error : function(xhr, ajaxOptions, thrownError) {
-		}
+	// $.ajax({
+	// type : "GET",
+	// url : "https://www.googleapis.com/language/translate/v2,
+	// dataType : 'jsonp',
+	// cache : false,
+	// contentType : "application/x-www-form-urlencoded; charset=UTF-8",
+	// data : "key="+translateKey+"&source=en&target=de&q=Hello",
+	// success : function(res) {
+	// console.log('res', res.data.translations[0].translatedText);
+	// },
+	// error : function(xhr, ajaxOptions, thrownError) {
+	// }
+	// });
+
+	$.get("https://www.googleapis.com/language/translate/v2", {
+		key : translateKey,
+		source : "en",
+		target : "fr",
+		q : "hello"
+	}, function(res) {
+		console.log(res.data.translations[0].translatedText);
+	}, "json").fail(function(jqXHR, textStatus, errorThrown) {
+		alert("error :" + errorThrown);
 	});
 }
 
 function getZodiac(day, month) {
-	if ((month == 1 && day <= 20) || (month == 12 && day >= 22)) {return zodiacSigns.capricorn;}
-	else if ((month == 1 && day >= 21) || (month == 2 && day <= 18)) {return zodiacSigns.aquarius;} 
-	else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {return zodiacSigns.pisces;} 
-	else if ((month == 3 && day >= 21) || (month == 4 && day <= 20)) {return zodiacSigns.aries;} 
-	else if ((month == 4 && day >= 21) || (month == 5 && day <= 20)) {return zodiacSigns.taurus;} 
-	else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {return zodiacSigns.gemini;} 
-	else if ((month == 6 && day >= 22) || (month == 7 && day <= 22)) {return zodiacSigns.cancer;} 
-	else if ((month == 7 && day >= 23) || (month == 8 && day <= 23)) {return zodiacSigns.leo;} 
-	else if ((month == 8 && day >= 24) || (month == 9 && day <= 23)) {return zodiacSigns.virgo;} 
-	else if ((month == 9 && day >= 24) || (month == 10 && day <= 23)) {return zodiacSigns.libra;}
-	else if ((month == 10 && day >= 24) || (month == 11 && day <= 22)) {return zodiacSigns.scorpio;}
-	else if ((month == 11 && day >= 23) || (month == 12 && day <= 21)) {return zodiacSigns.sagittarius;}
+	if ((month == 1 && day <= 20) || (month == 12 && day >= 22)) {
+		return zodiacSigns.capricorn;
+	} else if ((month == 1 && day >= 21) || (month == 2 && day <= 18)) {
+		return zodiacSigns.aquarius;
+	} else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+		return zodiacSigns.pisces;
+	} else if ((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
+		return zodiacSigns.aries;
+	} else if ((month == 4 && day >= 21) || (month == 5 && day <= 20)) {
+		return zodiacSigns.taurus;
+	} else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+		return zodiacSigns.gemini;
+	} else if ((month == 6 && day >= 22) || (month == 7 && day <= 22)) {
+		return zodiacSigns.cancer;
+	} else if ((month == 7 && day >= 23) || (month == 8 && day <= 23)) {
+		return zodiacSigns.leo;
+	} else if ((month == 8 && day >= 24) || (month == 9 && day <= 23)) {
+		return zodiacSigns.virgo;
+	} else if ((month == 9 && day >= 24) || (month == 10 && day <= 23)) {
+		return zodiacSigns.libra;
+	} else if ((month == 10 && day >= 24) || (month == 11 && day <= 22)) {
+		return zodiacSigns.scorpio;
+	} else if ((month == 11 && day >= 23) || (month == 12 && day <= 21)) {
+		return zodiacSigns.sagittarius;
+	}
 }
+
+/*
+ No. Language Name   Native Language Name    Code
+
+ 1   Afrikaans   Afrikaans           af
+ 2   Albanian    Shqip                   sq
+ 3   Arabic          عربي            ar
+ 4   Armenian    Հայերէն                 hy
+ 5   Azerbaijani آذربایجان دیلی          az
+ 6   Basque          Euskara                 eu
+ 7   Belarusian  Беларуская          be
+ 8   Bulgarian   Български           bg
+ 9   Catalan         Català                  ca
+ 10  Chinese (Simp.) 中文简体                    zh-CN
+ 11  Chinese (Trad.) 中文繁體                    zh-TW
+ 12  Croatian    Hrvatski            hr
+ 13  Czech           Čeština                 cs
+ 14  Danish          Dansk                   da
+ 15  Dutch           Nederlands          nl
+ 16  English         English                 en
+ 17  Estonian    Eesti keel          et
+ 18  Filipino    Filipino            tl
+ 19  Finnish         Suomi                   fi
+ 20  French          Français            fr
+ 21  Galician    Galego                  gl
+ 22  Georgian    ქართული         ka
+ 23  German          Deutsch                 de
+ 24  Greek           Ελληνικά            el
+ 25  Haitian Creole  Kreyòl ayisyen          ht
+ 26  Hebrew          עברית                   iw
+ 27  Hindi             हिन्दी                       hi
+ 28  Hungarian   Magyar                  hu
+ 29  Icelandic   Íslenska            is
+ 30  Indonesian  Bahasa Indonesia    id
+ 31  Irish           Gaeilge             ga
+ 32  Italian     Italiano            it
+ 33  Japanese    日本語             ja
+ 34  Korean      한국어                 ko
+ 35  Latvian     Latviešu            lv
+ 36  Lithuanian  Lietuvių kalba      lt
+ 37  Macedonian  Македонски          mk
+ 38  Malay       Malay               ms
+ 39  Maltese     Malti               mt
+ 40  Norwegian   Norsk               no
+ 41  Persian     فارسی               fa
+ 42  Polish      Polski              pl
+ 43  Portuguese  Português           pt
+ 44  Romanian    Română              ro
+ 45  Russian     Русский             ru
+ 46  Serbian     Српски              sr
+ 47  Slovak          Slovenčina          sk
+ 48  Slovenian   Slovensko           sl
+ 49  Spanish     Español             es
+ 50  Swahili     Kiswahili           sw
+ 51  Swedish     Svenska             sv
+ 52      Thai            ไทย                     th
+ 53  Turkish     Türkçe              tr
+ 54  Ukrainian   Українська          uk
+ 55  Urdu        اردو                    ur
+ 56  Vietnamese  Tiếng Việt          vi
+ 57  Welsh       Cymraeg             cy
+ 58  Yiddish     ייִדיש                  yi
+ */
