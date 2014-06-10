eaa1ce3eb6723b0f9aa18ebd59232774ff063fc1
avishayhajbi <avishayhajbi@gmail.com>
2014-06-10 15:13:21 +0300

    page astrology

--- includes/js/db_files.js
+++ includes/js/db_files.js
@@ -3,62 +3,62 @@ var zodiacSigns = {
 'capricorn' : {
 'name':'Capricorn',
 'background':'includes/images/zodiacBackgrounds/capricorn.jpg',
-'icon':'../images/zodiacIcons/capricorn.jpg'
+'icon':'../images/zodiacIcons/capricorn.png'
 },
 'aquarius' : {
 'name':'Aquarius',
 'background':'includes/images/zodiacBackgrounds/aquarius.jpg',
-'icon':'../images/zodiacIcons/aquarius.jpg'
+'icon':'includes/images/zodiacIcons/aquaruis.png'
 },
 'pisces' : {
 'name':'Pisces',
 'background':'includes/images/zodiacBackgrounds/pisces.jpg',
-'icon':'../images/zodiacIcons/pisces.jpg'
+'icon':'includes//images/zodiacIcons/pisces.png'
 },
 'aries' : {
 'name':'Aries',
 'background':'includes/images/zodiacBackgrounds/aries.jpg',
-'icon':'../images/zodiacIcons/aries.jpg'
+'icon':'includes//images/zodiacIcons/aries.png'
 },
 'taurus' : {
 'name':'Taurus',
 'background':'includes/images/zodiacBackgrounds/taurus.jpg',
-'icon':'../images/zodiacIcons/taurus.jpg'
+'icon':'includes//images/zodiacIcons/taurus.png'
 },
 'gemini' : {
 'name':'Gemini',
 'background':'includes/images/zodiacBackgrounds/gemini.jpg',
-'icon':'../images/zodiacIcons/gemini.jpg'
+'icon':'includes//images/zodiacIcons/gemini.png'
 },
 'cancer' : {
 'name':'Cancer',
 'background':'includes/images/zodiacBackgrounds/cancer.jpg',
-'icon':'../images/zodiacIcons/cancer.jpg'
+'icon':'includes//images/zodiacIcons/cancer.png'
 },
 'leo' : {
 'name':'Leo',
 'background':'includes/images/zodiacBackgrounds/leo.jpg',
-'icon':'../images/zodiacIcons/leo.jpg'
+'icon':'includes//images/zodiacIcons/leo.png'
 },
 'virgo' : {
 'name':'Virgo',
 'background':'includes/images/zodiacBackgrounds/virgo.jpg',
-'icon':'../images/zodiacIcons/virgo.jpg'
+'icon':'includes//images/zodiacIcons/virgo.png'
 },
 'libra' : {
 'name':'Libra',
 'background':'includes/images/zodiacBackgrounds/libra.jpg',
-'icon':'../images/zodiacIcons/libra.jpg'
+'icon':'includes//images/zodiacIcons/libra.png'
 },
 'scorpio' : {
 'name':'Scorpio',
 'background':'includes/images/zodiacBackgrounds/scorpio.jpg',
-'icon':'../images/zodiacIcons/scorpio.jpg'
+'icon':'includes//images/zodiacIcons/scorpio.png'
 },
 'sagittarius' : {
 'name':'Sagittarius',
 'background':'includes/images/zodiacBackgrounds/sagittarius.jpg',
-'icon':'../images/zodiacIcons/sagittarius.jpg'
+'icon':'includes//images/zodiacIcons/sagittarius.png'
 }
 };
 
