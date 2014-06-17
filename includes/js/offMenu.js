1a8ca299a2e9bfa6428edb092415597ba1d3f9d5
ofir agahi <iofirag@gmail.com>
2014-06-18 02:03:13 +0300

    design statistics nav canvas

--- includes/js/offMenu.js
+++ includes/js/offMenu.js
@@ -22,22 +22,7 @@ function toggleNav_Left() {
         // Do things on Nav Open
         $('#site-wrapper').addClass('show-nav-left');
         
-        //find all true obj
-		trueObj = [];
-		database.forEach(function(obj) {
-			if (obj.like)  trueObj.push(obj);
-		});
-		console.table(trueObj);
-		
-		//put all true obj into #true_comments
-		trueObj.forEach(function(obj){
-			name = obj.name;
-			time = obj.time;
-			country = obj.country;
-			comment = obj.comment;
-			console.log("name="+name +"  time="+time +"  country="+country  +"  comment="+comment);																	//	<section class='comments_wall_timeIcon'><section>
-			$('#true_wall').prepend("<section class='user_comment'>    <section>"+name+"</section>    <section>"+time+"w</section>   <section>"+country+"</section>    <section>pic</section>    </section>");
-		});
+        buildCommentPage(true);
     }
     //$('#site-wrapper').toggleClass('show-nav');
 }
@@ -71,22 +56,47 @@ function toggleNav_right() {
         // Do things on Nav Open
         $('#site-wrapper').addClass('show-nav-right');
                 
-        //find all true obj
-		falseObj = [];
-		database.forEach(function(obj) {
-			if (!obj.like)  falseObj.push(obj);
-		});
-		console.table(falseObj);
-		
-		//put all true obj into #true_comments
-		falseObj.forEach(function(obj){
-			name = obj.name;
-			time = obj.time;
-			country = obj.country;
-			comment = obj.comment;
-			console.log("name="+name +"  time="+time +"  country="+country  +"  comment="+comment);
-			$('#false_wall').prepend("<section class='user_comment'>    <section>"+name+"</section>    <section>"+time+"w</section>   <section>"+country+"</section>    <section>pic</section>    </section>");
-		});
+         buildCommentPage(false);
     }
     //$('#site-wrapper').toggleClass('show-nav');
+}
+
+
+
+
+function buildCommentPage(bool){
+	console.log('buildCommentPage(false);');
+	userList = [];
+	
+	//find all false users
+	database.forEach(function(obj) {
+		if (obj.like == bool && obj.country == userCountry)  userList.push(obj);
+	});
+	console.table(userList);
+	
+	//put all true obj into #true_comments
+	userList.forEach(function(obj){
+		name = obj.name;
+		time = obj.time;
+		country = obj.country;
+		comment = obj.comment;
+		console.log("name="+name +"  time="+time +"  country="+country  +"  comment="+comment);
+		
+		//post and save 'id' specific from millisecond for every one of post
+		var d = new Date();
+		post = d.getMilliseconds();
+		$('#'+bool+'_wall').prepend("<section class='user_comment'>    <section class='comments_name'>"+name+"</section>    <section class='comments_time'><section class='comments_time_timeIcon'></section><section class='comments_time_timeNumber'>"+time+"w</section><div class='clear'></div></section>  <br>  <section class='comments_location'><section class='comments_location_icon'></section><section class='comments_country_name'>"+country+"</section><div class='clear'></div></section>      <section class='pic_comments' id="+post+"></section>   <br>   <button name='button' class='button_likes'>Likes</button>   <button name='button' class='button_comment'>Comment</button>   </section>");
+		
+		var random;
+		//the random image number
+		if (bool==true) random = getRandomInt(1, 17);	//true = 1,2,3 images
+		else 			random = getRandomInt(1, 11);	//false = 1,2 images
+		$('#'+post).css('background' , "url('includes/images/commentsPhotos/"+bool+random+".jpg') no-repeat");
+		// $('#'+post).css('width' , "500px");
+		// $('#'+post).css('height' , "340px");
+	});
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
